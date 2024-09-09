38afe851dfba2bda70d995575eaa55d603d5a9c1
Armina104 <143567451+Armina104@users.noreply.github.com>
2024-09-09 08:44:47 +0800

    Add files via upload

---
+++ MasohodSolution/TABLE/WORKS_ON.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[WORKS_ON]
+(
+	[Essn] CHAR(9) NOT NULL PRIMARY KEY, 
+    [Pno] INT NOT NULL, 
+    [Hours] DECIMAL(3, 1) NOT NULL,
+    PRIMARY KEY (Essn,Pno),
+    FOREIGN KEY (Essn) REFERENCES EMPLOYEE(Ssn),
+    FOREIGN KEY (Pno) REFERENCES PROJECT(Pnumber),
+     [constraint] [PK_Works_Essn] PRIMARY KEY clustered ([Essn]),
+    --constraint [UX_Works_Pno] unique nonclustered ([Pno])
+     UNIQUE(Essn),
+    FOREIGN KEY (Pno) REFERENCES EMPLOYEE (Pno),
+);
+Go 
+
+create unique nonclustered index [UX_Works_Essn] ON [dba].[Works]([Essn]),
+
+Go
+create nonclustered index[IX_Works_Pno] ON [dba].[Works]([Pno] ASC)
+
